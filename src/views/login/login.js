81f661581c2f23117e8ae5fb5452dd85655426c4
tanyasalsa <salsabillatanya@gmail.com>
2025-08-06 11:43:51 +0700

    update login module

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -10,10 +10,10 @@ export default function Login() {
       {/* TOPBAR */}
       <div className={styles.topbar}>
         <Image
-          src="/assets/D'ONE Putih.png"
-          alt="D'ONE Logo Putih"
-          width={90}
-          height={90}
+          src="/assets/Logo BI Putih.png"
+          alt="Logo BI Putih"
+          width={220}
+          height={110}
           className={styles.logoOnly}
           priority
         />
@@ -30,20 +30,12 @@ export default function Login() {
       {/* CARD */}
       <div className={styles.card}>
         <div className={styles.cardHeader}>
-          <Image
-            src="/assets/BI_Logo.png"
-            alt="Bank Indonesia"
-            width={260}
-            height={60}
-            className={styles.cardBankLogo}
-            priority
-          />
-          <div className={styles.welcome}>Selamat datang di</div>
+          
         </div>
         <div className={styles.logoDoneWrapper}>
           <Image
-            src="/assets/D'ONE.png"
-            alt="D'ONE Logo"
+            src="/assets/Logo D'ONE.png"
+            alt="Logo D'ONE"
             width={220}
             height={110}
             className={styles.logoLarge}
@@ -51,7 +43,7 @@ export default function Login() {
           />
         </div>
         <div className={styles.cardTitle}>
-          Digital One Order<br />By Bank Indonesia
+          One Ordering System<br />By KPw BI Provinsi Jawa Timur
         </div>
         <Link href="/Signin/hal-sign" passHref legacyBehavior>
           <button className={styles.button}>Masuk</button>
@@ -65,7 +57,7 @@ export default function Login() {
           <button className={styles.button}>Masuk Sebagai Admin</button>
         </Link>
         <div className={styles.signupText}>
-          Belum punya akun?{' '}
+          Belum memiliki akun?{' '}
           <Link href="/SignUp/hal-signup" passHref legacyBehavior>
             <span className={styles.signupLink}>Daftar di sini</span>
           </Link>
