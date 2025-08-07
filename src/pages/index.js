67007aed2a09aa95f88e67f1b19bf88c33e382fc
tanyasalsa <salsabillatanya@gmail.com>
2025-08-07 09:03:26 +0700

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,22 +25,23 @@ export default function LandingPage() {
         <div className="lp-overlay" />
         {/* Card */}
         <div className="lp-card">
+          <h1>
+            <span className="lp-welcome">Selamat Datang di</span>
+          </h1>
+
           <div className="logo-landing-wrapper">
             <Image
               src="/assets/Logo D'ONE.png"
-              alt="B'ONE Logo"
+              alt="BI.ONE Logo"
               fill
               style={{ objectFit: 'contain' }}
-              sizes="170px"
+              sizes="200px"
               priority
             />
           </div>
-          <h1>
-            <span className="lp-welcome">Selamat Datang di</span><br />
-            <span className="lp-brand">BI&#39;ONE</span>
-          </h1>
+
           <p className="lp-desc">
-             One Ordering System<br />By KPw BI Provinsi Jawa Timur
+            One Ordering System<br />By KPw BI Provinsi Jawa Timur
           </p>
         </div>
       </div>
@@ -78,14 +79,14 @@ export default function LandingPage() {
           text-align: center;
           background: rgba(255,255,255,0.98);
           border-radius: 22px;
-          padding: 60px 80px 60px 80px;
+          padding: 60px 80px;
           box-shadow: 0 8px 38px 0 rgba(70,90,150,0.14), 0 1.5px 6px 0 rgba(70,90,150,0.04);
           animation: popin 1.1s cubic-bezier(.39,1.53,.56,.91);
           transition: box-shadow .3s;
         }
         .logo-landing-wrapper {
-          width: 150px;
-          height: 82px;
+          width: 200px;
+          height: 100px;
           margin: 0 auto 18px auto;
           display: flex;
           align-items: center;
@@ -97,8 +98,8 @@ export default function LandingPage() {
           font-weight: 600;
           font-size: 1.6rem;
           letter-spacing: 0.02em;
-          display: inline-block;
-          font-family: inherit;
+          display: block;
+          margin-bottom: 20px;
         }
         .lp-brand {
           color: #2F4D8E;
@@ -125,13 +126,13 @@ export default function LandingPage() {
         }
         @media (max-width: 800px) {
           .lp-card {
-            padding: 32px 6px 24px 6px;
+            padding: 32px 16px 24px 16px;
             border-radius: 14px;
           }
           .logo-landing-wrapper {
             width: 90px;
             max-width: 250px;
-            height: 16px;
+            height: 60px;
             max-height: 70px;
           }
           .lp-welcome { font-size: 1.14rem;}
